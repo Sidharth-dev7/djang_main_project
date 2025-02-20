a666eb2128a63b027f21507a0dab76b5dd7b0371
Adithya-Sidharth <adithyasid2002@gmail.com>
2025-02-20 12:27:14 +0530

    Model

--- myapp/models.py
+++ myapp/models.py
@@ -5,4 +5,6 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=30)
     contact = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=20)
 
