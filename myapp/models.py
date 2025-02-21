898b4a012fbea15d286c18360f626f86d89c7132
Adithya-Sidharth <adithyasid2002@gmail.com>
2025-02-21 11:47:23 +0530

    Views

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class Customer(models.Model):
     name = models.CharField(max_length=30)
     contact = models.CharField(max_length=15)
     email = models.EmailField(unique=True)
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Customer, Car
 
 # Create your views here.
+
+def registration(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        contact = request.POST.get('contact')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
