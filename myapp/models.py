21237e85b3098c0c4227ae358d86d706f50a193d
Adithya-Sidharth <adithyasid2002@gmail.com>
2025-02-21 11:47:47 +0530

    Views

--- myapp/models.py
+++ myapp/models.py
@@ -9,7 +9,7 @@ class Customer(models.Model):
     password = models.CharField(max_length=20)
 
 class Car(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Customer, on_delete=models.CASCADE)
     manufacturer = models.CharField(max_length=20)
     auto_model = models.CharField(max_length=20)
     reg_plate = models.CharField(max_length=10)
